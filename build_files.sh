5e5e08193413171120582789760f6bf2075bcbd6
a3laxi <mad.aalachi@gmail.com>
2023-04-01 03:46:17 +0200

    DEEPLOY

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 # build_files.sh
 pip install django-cors-headers
 pip install djangorestframework-simplejwt
+pip install psycopg2-binary
 
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
